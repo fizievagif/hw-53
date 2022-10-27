e742f4911eabfb7c9e7f476c4e198fc9e4ba75ba
Fiziev Vagif <vagasuper560469@gmail.com>
2022-10-28 00:57:42 +0600

    Добавил компонент AddTaskForm, описал внешний вид
    Почистил проект от ненужных файлов. Добавил .idea в .gitignore

---
+++ src/AddTaskFrom.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+interface AddTaskFormProps {
+  value: React.ChangeEventHandler<HTMLInputElement>,
+  task: React.MouseEventHandler
+}
+
+const AddTaskForm: React.FC<AddTaskFormProps> = props => {
+  return (
+    <form className="todo-block">
+      <input
+        type="text"
+        placeholder="Add new task"
+        onChange={props.value}
+      />
+      <button
+        className="todo-btn"
+        type="submit"
+        onClick={props.task}
+      >Add</button>
+    </form>
+  )
+};
+
+export default AddTaskForm;
